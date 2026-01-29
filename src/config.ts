5e9cf6fdb6d55a5a28f7ad16249629a7f742b8e3
Ilya Lebedev <ilyalebedev1@yandex.ru>
2026-01-29 17:42:32 +0700

    fix: main page

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Ilya Lebedev | Personal website';
-export const SITE_DESCRIPTION = 'Ruby developer, entrepreneur, traveller';
+export const SITE_DESCRIPTION = 'Software engineer, technical founder, digital nomad';
